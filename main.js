8feff37e8a13e919bca5f0119d4a0275ce8cb2f5
ashan36@gmail.com <ashan36@gmail.com>
2019-03-07 18:23:08 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const greetings = require("./greeting.js");
+const math = require("./basic-math.js");
+
+// "Hello"
+greetings.greetingEnglish();
+
+// "Konichiwa"
+greetings.greetingJapanese();
+
+greetings.greetingSpanish();
+
+greetings.greetingIcelandic();
+
+console.log(math.add(1, 2));
+
+console.log(math.subtract(3, 4));
